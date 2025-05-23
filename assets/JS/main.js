67f141f96b4c9065c1b3a3cacfa8630f7b23a9a1
mod-prashant <prashantuniyal2002@gmail.com>
2025-05-23 17:02:46 +0530

    Partially fixed the share button for mobile

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,27 +1,20 @@
 const shareBtn = document.getElementById("shareBtn");
 const tooltip = document.getElementById("shareTooltip");
-
-shareBtn.addEventListener("click", () => {
-  shareBtn.classList.toggle("active");
-  tooltip.classList.toggle("active");
-});
-
-/*
-const shareBtn = document.getElementById("shareBtn");
-const tooltip = document.getElementById("shareTooltip");
 const authorInfo = document.getElementById("authorInfo");
 const shareTooltip = document.getElementById("shareTooltip");
+const shareBOX = document.getElementById("shareBOX");
 
-// Toggle based on screen width
+
 shareBtn.addEventListener("click", () => {
   const isMobile = window.innerWidth <= 768;
 
   if (isMobile) {
-    authorInfo.classList.toggle("hidden");
+    authorInfo.classList.toggle("hide");
     shareTooltip.classList.toggle("active");
+    shareBOX.classList.toggle("style");
+    shareBtn.classList.toggle("active");
   } else {
     tooltip.classList.toggle("active");
     shareBtn.classList.toggle("active");
   }
 });
-*/
